39565db725fad22a463c4ec53ea7cd9acb89878a
Malte <maltebjoerklund@gmail.com>
2023-06-02 21:04:47 +0200

    Update script.js

--- environments/environment-03/script.js
+++ environments/environment-03/script.js
@@ -1,1 +1,48 @@
 "use strict";
+
+window.addEventListener("load", start);
+
+const products = [
+  { name: "product3", price: 2, inStock: true },
+  { name: "product2", price: 4, inStock: false },
+  { name: "product1", price: 6, inStock: true },
+];
+
+function start() {
+  showSortedProducts();
+  createProduct("product4", 8, false);
+  createProduct("product5", 10, true);
+  createProduct("product6", 12, false);
+}
+
+function showSortedProducts() {
+  if (document.querySelector("#select-sort-by").value === "inStock") {
+    products.sort((a, b) => b.inStock - a.inStock);
+  }
+  const listContainer = document.querySelector("#list-container");
+  listContainer.innerHTML = "";
+  listContainer.insertAdjacentHTML(
+    "beforeend",
+    /* html */ `
+        <ul></ul>
+    `
+  );
+  for (const product of products) {
+    document.querySelector("#list-container ul").insertAdjacentHTML(
+      "beforeend",
+      /* html */ `
+         <li>Product: ${product.name} | Price: ${product.price} | In Stock: ${product.inStock}</li>
+      `
+    );
+  }
+}
+
+function createProduct(name, price, inStock) {
+  const newProduct = {
+    name: name,
+    price: price,
+    inStock: inStock,
+  };
+  products.push(newProduct);
+  showSortedProducts();
+}
