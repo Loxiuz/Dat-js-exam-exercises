d75878967e4189534970c4c99aa408588d59db60
Malte <maltebjoerklund@gmail.com>
2023-06-02 19:34:19 +0200

    Update script.js

--- environments/environment-02/script.js
+++ environments/environment-02/script.js
@@ -2,4 +2,43 @@
 
 window.addEventListener("load", start);
 
-function start() {}
+const animals = [
+  { name: "King", type: "dog", age: 7 },
+  { name: "Buster", type: "cat", age: 4 },
+  { name: "Albert", type: "horse", age: 8 },
+];
+
+function start() {
+  showAnimals();
+  createAnimal("Navn1", "cat", 5);
+  createAnimal("Navn2", "dragon", 10);
+  createAnimal("Navn3", "horse", 2);
+}
+
+function showAnimals() {
+  animals.sort((a, b) => a.age - b.age);
+  const tbody = document.querySelector("tbody");
+  tbody.innerHTML = "";
+  for (const animal of animals) {
+    tbody.insertAdjacentHTML(
+      "beforeend",
+      /* html */ `
+        <tr>
+          <td>${animal.name}</td>
+          <td>${animal.type}</td>
+          <td>${animal.age}</td>
+        </tr>
+        `
+    );
+  }
+}
+
+function createAnimal(name, type, age) {
+  const newAnimal = {
+    name: name,
+    type: type,
+    age: age,
+  };
+  animals.push(newAnimal);
+  showAnimals();
+}
