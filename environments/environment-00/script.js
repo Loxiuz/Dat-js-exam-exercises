12ebf8d90e9b75d9b2983470e704c5cdd4dda7b9
Malte <maltebjoerklund@gmail.com>
2023-06-02 16:06:37 +0200

    Update script.js

--- environments/environment-00/script.js
+++ environments/environment-00/script.js
@@ -1,1 +1,15 @@
 "use strict";
+
+window.addEventListener("load", start);
+
+function start() {
+  document.querySelector("#result_failure").classList.add("hide");
+  document.querySelector("#result_success").classList.add("hide");
+
+  document.querySelector("#btn-tekst").addEventListener("click", clicked);
+}
+
+function clicked() {
+  console.log("Det virker!");
+  document.querySelector("#result_success").classList.add("show");
+}
