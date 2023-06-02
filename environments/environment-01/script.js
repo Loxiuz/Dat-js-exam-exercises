96a4a942ffaaa551af4322bb66a2320566eca1de
Malte <maltebjoerklund@gmail.com>
2023-06-02 16:36:21 +0200

    Update script.js

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -1,2 +1,32 @@
 "use strict";
 
+window.addEventListener("load", start);
+
+async function start() {
+  console.log(await getData());
+  await showUsers();
+}
+
+async function getData() {
+  const users = [];
+  const response = await fetch("users.json");
+  const data = await response.json();
+  for (const key in data) {
+    users.push(data[key]);
+  }
+  return users;
+}
+
+async function showUsers() {
+  const users = await getData();
+  for (const user of users) {
+    if (user.role === "admin") {
+      document.querySelector("#userlist").insertAdjacentHTML(
+        "beforeend",
+        /* html */ `
+            <li><p>Navn: ${user.name} | Aktiv: ${user.active}</p></li>
+        `
+      );
+    }
+  }
+}
