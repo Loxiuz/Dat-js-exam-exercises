3e9d424ceace50babe0c1b13333b1f2c44ce2d83
Malte <maltebjoerklund@gmail.com>
2023-06-02 20:45:56 +0200

    Update script.js

--- environments/environment-03/script.js
+++ environments/environment-03/script.js
@@ -1,1 +1,47 @@
 "use strict";
+
+window.addEventListener("load", start);
+
+const products = [
+  { name: "product1", price: 2, inStock: true },
+  { name: "product2", price: 4, inStock: false },
+  { name: "product3", price: 6, inStock: true },
+];
+
+function start() {
+  showProducts();
+  createProdukt("product4", 8, false);
+  createProdukt("product5", 10, true);
+  createProdukt("product6", 12, true);
+}
+
+function showProducts() {
+  const listContainer = document.querySelector("#list-container");
+  listContainer.innerHTML = "";
+  listContainer.insertAdjacentHTML(
+    "beforeend",
+    /* html */ `
+        <ul></ul>
+   `
+  );
+  for (const product of products) {
+    if (product.inStock === true) {
+      document.querySelector("#list-container ul").insertAdjacentHTML(
+        "beforeend",
+        /* html */ `
+            <li>Product: ${product.name} | Price: ${product.price} | In Stock: ${product.inStock}</li>
+        `
+      );
+    }
+  }
+}
+
+function createProdukt(name, price, inStock) {
+  const newProduct = {
+    name: name,
+    price: price,
+    inStock: inStock,
+  };
+  products.push(newProduct);
+  showProducts();
+}
