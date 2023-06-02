bd6a3f96951d373a9b5dba13a88178ce9f7a3e90
Malte <maltebjoerklund@gmail.com>
2023-06-02 16:50:35 +0200

    Update script.js

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -1,2 +1,53 @@
 "use strict";
 
+window.addEventListener("load", start);
+
+async function start() {
+  await showUsers();
+}
+
+async function getData() {
+  const users = [];
+  const response = await fetch("users.json");
+  const data = await response.json();
+  for (const key in data) {
+    users.push(data[key]);
+  }
+  console.log(users);
+  return users;
+}
+
+async function showUsers() {
+  const users = await getData();
+  for (const user of users) {
+    document.querySelector("#userlist").insertAdjacentHTML(
+      "beforeend",
+      /* html */ `
+        <li>Name: ${user.name} | Aktiv: ${user.active} | Rolle: ${user.role}</li>
+      `
+    );
+  }
+  showRolesCount(users);
+}
+
+function showRolesCount(users) {
+  let adminCount = 0;
+  let userCount = 0;
+  let guestCount = 0;
+  for (const user of users) {
+    switch (user.role) {
+      case "admin":
+        adminCount++;
+        break;
+      case "user":
+        userCount++;
+        break;
+      case "guest":
+        guestCount++;
+        break;
+    }
+  }
+  document.querySelector("#admin-count").textContent = adminCount;
+  document.querySelector("#user-count").textContent = userCount;
+  document.querySelector("#guest-count").textContent = guestCount;
+}
