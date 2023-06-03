0a9a805a9902d27264e85e7cf3887f839a5a0f0c
Malte <maltebjoerklund@gmail.com>
2023-06-03 15:13:46 +0200

    exercise done

--- environments/environment-05/script.js
+++ environments/environment-05/script.js
@@ -1,1 +1,25 @@
 "use strict";
+
+window.addEventListener("load", start);
+
+import { courses } from "./courses.js";
+
+function start() {
+  showCourses();
+}
+
+function showCourses() {
+  sortByPoints();
+  for (const course of courses) {
+    document.querySelector("#courses-list").insertAdjacentHTML(
+      "beforeend",
+      /* html */ `
+        <li>Name: ${course.name} | ECTS-Points: ${course.ectsPoints}</li>    
+      `
+    );
+  }
+}
+
+function sortByPoints() {
+  courses.sort((a, b) => a.ectsPoints - b.ectsPoints);
+}
