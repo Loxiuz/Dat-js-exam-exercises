ee1c2ede6d6d42bdc014f925657f5cbdd5d075d8
Malte <maltebjoerklund@gmail.com>
2023-06-03 14:57:16 +0200

    exercise done

--- environments/environment-04/script.js
+++ environments/environment-04/script.js
@@ -1,1 +1,34 @@
 "use strict";
+
+window.addEventListener("load", start);
+
+import { teachers } from "./teachers.js";
+
+function start() {
+  showTeachers();
+  createTeacher("teacher1", "email1@email.com");
+  createTeacher("teacher2", "email2@email.com");
+  createTeacher("teacher3", "email3@email.com");
+}
+
+function showTeachers() {
+  const teachersList = document.querySelector("#teachers-list");
+  teachersList.innerHTML = "";
+  for (const teacher of teachers) {
+    teachersList.insertAdjacentHTML(
+      "beforeend",
+      /* html */ `
+        <li>Name: ${teacher.name} | Email: ${teacher.email}</li>
+      `
+    );
+  }
+}
+
+function createTeacher(name, email) {
+  const newTeacher = {
+    name: name,
+    email: email,
+  };
+  teachers.push(newTeacher);
+  showTeachers();
+}
