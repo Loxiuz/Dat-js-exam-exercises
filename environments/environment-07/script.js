e625d572c71269bf1151c6cd16e8c7fe56f5f36c
Malte <maltebjoerklund@gmail.com>
2023-06-05 18:15:22 +0200

    Update script.js

--- environments/environment-07/script.js
+++ environments/environment-07/script.js
@@ -1,1 +1,40 @@
 "use strict";
+
+window.addEventListener("load", start);
+
+const students = [];
+
+function start() {
+  document
+    .querySelector("#create-student-form button")
+    .addEventListener("click", createStudent);
+}
+
+function createStudent(event) {
+  event.preventDefault();
+  const createForm = document.querySelector("#create-student-form");
+  const newStudent = {
+    name: createForm.name.value,
+    email: createForm.email.value,
+    age: createForm.age.value,
+  };
+  students.push(newStudent);
+  console.log(students);
+
+  students.sort((a, b) => a.age - b.age);
+
+  const tbody = document.querySelector("#students-table-body");
+  tbody.innerHTML = "";
+  for (const student of students) {
+    tbody.insertAdjacentHTML(
+      "beforeend",
+      /* html */ `
+        <tr>
+          <td>${student.name}</td>
+          <td>${student.email}</td>
+          <td>${student.age}</td>
+        </tr>
+      `
+    );
+  }
+}
