9695e5e80b3f17c3b0879ad55672edcabbb7f4b2
Malte <maltebjoerklund@gmail.com>
2023-06-08 16:47:09 +0200

    exercise 15 done

--- environments/environment-06/script.js
+++ environments/environment-06/script.js
@@ -2,6 +2,8 @@
 
 window.addEventListener("load", start);
 
+const basket = [];
+
 function start() {
   showProducts();
 }
@@ -20,17 +22,43 @@ async function getProducts() {
 
 async function showProducts() {
   const products = await getProducts();
-  for (const product of products) {
+  for (let i = 0; i < products.length; i++) {
     document.querySelector("#products").insertAdjacentHTML(
       "beforeend",
       /* html */ `
         <article>
-           <h3>${product.name}</h3>
-           <p>vægt: ${product.weight} g</p>
-           <p>pris: ${product.price},-</p>
-           <button>Læg i kurv</button>
+           <h3>${products[i].name}</h3>
+           <p>vægt: ${products[i].weight} g</p>
+           <p>pris: ${products[i].price},-</p>
+           <button id="btn-${i}">Læg i kurv</button>
+        </article>
+      `
+    );
+    document.querySelector(`#btn-${i}`).addEventListener("click", () => {
+      addToBasket(products[i]);
+      showBasket();
+      console.log(basket);
+    });
+  }
+}
+
+function showBasket() {
+  const basketHtml = document.querySelector("#basket");
+  basketHtml.innerHTML = "";
+  for (let i = 0; i < basket.length; i++) {
+    basketHtml.insertAdjacentHTML(
+      "beforeend",
+      /* html */ `
+        <article>
+           <h3>${basket[i].name}</h3>
+           <p>vægt: ${basket[i].weight} g</p>
+           <p>pris: ${basket[i].price},-</p>
         </article>
       `
     );
   }
+}
+
+function addToBasket(product) {
+  basket.push(product);
 }
