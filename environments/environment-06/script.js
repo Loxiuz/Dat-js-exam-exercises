9f5f17bfad9dd2af42eb2d3db82ac1a841ef41b1
Malte <maltebjoerklund@gmail.com>
2023-06-08 16:34:21 +0200

    Update script.js

--- environments/environment-06/script.js
+++ environments/environment-06/script.js
@@ -2,4 +2,35 @@
 
 window.addEventListener("load", start);
 
-function start() {}
+function start() {
+  showProducts();
+}
+
+async function getProducts() {
+  const products = [];
+  const response = await fetch("products.json");
+  const data = await response.json();
+
+  for (const key in data) {
+    products.push(data[key]);
+  }
+
+  return products;
+}
+
+async function showProducts() {
+  const products = await getProducts();
+  for (const product of products) {
+    document.querySelector("#products").insertAdjacentHTML(
+      "beforeend",
+      /* html */ `
+        <article>
+           <h3>${product.name}</h3>
+           <p>vægt: ${product.weight} g</p>
+           <p>pris: ${product.price},-</p>
+           <button>Læg i kurv</button>
+        </article>
+      `
+    );
+  }
+}
